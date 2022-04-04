053d227243779836d8df4441e512aa34ccf0d758
KushankVerma <69851040+KushankVerma@users.noreply.github.com>
2022-04-05 01:39:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,110 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import MapGL from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+import Geocoder from "react-map-gl-geocoder";
+
+// Please be a decent human and don't abuse my Mapbox API token.
+// If you fork this sandbox, replace my API token with your own.
+// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
+const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoic21peWFrYXdhIiwiYSI6ImNqcGM0d3U4bTB6dWwzcW04ZHRsbHl0ZWoifQ.X9cvdajtPbs9JDMG-CMDsA";
+
+class App extends Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    },
+    searchResultLayer: null
+  };
+
+  mapRef = React.createRef();
+  geocoderContainerRef = React.createRef();
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize);
+    this.resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resize);
+  }
+
+  resize = () => {
+    this.handleViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  };
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  handleGeocoderViewportChange = viewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    });
+  };
+
+  handleOnResult = event => {
+    this.setState({
+      searchResultLayer: new GeoJsonLayer({
+        id: "search-result",
+        data: event.result.geometry,
+        getFillColor: [255, 0, 0, 128],
+        getRadius: 1000,
+        pointRadiusMinPixels: 10,
+        pointRadiusMaxPixels: 10
+      })
+    });
+  };
+
+  render() {
+    const { viewport, searchResultLayer } = this.state;
+
+    return (
+      <div>
+        <div
+          ref={this.geocoderContainerRef}
+          style={{
+            height: 50,
+            background: "black",
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: 4
+          }}
+        />
+        <MapGL
+          ref={this.mapRef}
+          {...viewport}
+          onViewportChange={this.handleViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        >
+          <Geocoder
+            mapRef={this.mapRef}
+            containerRef={this.geocoderContainerRef}
+            onResult={this.handleOnResult}
+            onViewportChange={this.handleGeocoderViewportChange}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+          <DeckGL {...viewport} layers={[searchResultLayer]} />
+        </MapGL>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
